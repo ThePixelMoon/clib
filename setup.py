c0c0997ac3a3fffac0c44f33d65fc7966a2ff617
ThePixelMoon <yourlocalmoon7@gmail.com>
2024-11-13 20:31:08 +0200

    add

---
+++ setup.py
@@ -0,0 +1,10 @@
+# setup.py
+from setuptools import find_packages, setup
+
+setup(
+    name='clib',
+    packages=find_packages(),
+    version='0.1.1',
+    description='a compression library made in pure Python',
+    author='YourLocalMoon',
+)
